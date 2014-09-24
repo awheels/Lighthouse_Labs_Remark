97dd0d4364ac82148b7a3f67194a1613abb01fba
Andrew Wheeler <wheeler02@gmail.com>
2014-09-24 23:12:45 +0000

    Removed email required validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, :email, :password, presence: true
+  validates :username, :password, presence: true
   validates :username, :email, uniqueness: true
   has_many :comments
   has_many :documents
