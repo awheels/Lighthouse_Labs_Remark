a94e994260d1b9f0740d2716d28f8e95bc420c92
kingllama <dh.harris183@gmail.com>
2014-09-25 22:46:59 +0000

    Added support for posting documents

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   $('.editor').notebook();
-
+  console.log("doc loaded")
+  
   function convertToArr(set) {
     var results = [];
     set.each(function(index) {
       results.push(set[index].outerHTML);
     });
     return results;
-  }
-  
-  var input = $('.editor').find('p,h1,h2')
+  };
 
-  $('.submit-btn').on('click', function() {
+  $(".submit-btn").on( "click", function() {
+    console.log("clicked!")
+    var input = $('.editor').find('p,h1,h2');
     var paragraphs = convertToArr(input);
+    console.log(paragraphs)
     $.post('/docs', {p: paragraphs});
   });
 });
