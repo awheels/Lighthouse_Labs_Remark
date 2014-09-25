433075528f5628d6fb2cc66ac21cd08285364525
kingllama <dh.harris183@gmail.com>
2014-09-25 21:36:52 +0000

    Added the Jquery for the paragraph getter

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
   $('.editor').notebook();
+
+  function convertToArr(set) {
+    var results = [];
+    set.each(function(index) {
+      results.push(set[index].outerHTML);
+    });
+    return results;
+  }
+  
+  var input = $('.editor').find('p,h1,h2')
+
+  $('.submit-btn').on('click', function() {
+    var paragraphs = convertToArr(input);
+    $.post('/docs', {p: paragraphs});
+  });
 });
