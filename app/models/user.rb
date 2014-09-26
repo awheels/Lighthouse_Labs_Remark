f1d6baad15af4fcb052e4d22e025caef220ec628
kingllama <dh.harris183@gmail.com>
2014-09-26 20:19:15 +0000

    Added login redirects and better login functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   validates :username, :password, presence: true
   validates :username, :email, uniqueness: true
+  validate :matching_passwords
+  def matching_passwords #If the user's passwords don't match, The server (in users_actions.rb) will return password as false.
+    if password == false
+      errors.add(:passwords, "Must Match")
+    end
+  end
+
   has_many :comments
   has_many :documents
 end
